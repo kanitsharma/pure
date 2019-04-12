d15922465b907bb91b63f92aa0758e1dee2bd295
kanitsharma <kanitshrm09@gmail.com>
2019-04-12 16:24:27 +0530

    Updated configs

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,23 @@
-import babel from 'rollup-plugin-babel'
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import run from 'rollup-plugin-run';
+
+const dev = process.env.ROLLUP_WATCH === 'true';
 
 export default {
   input: 'src/index.js',
   output: {
     name: 'main',
     file: 'dist/main.js',
-    format: 'esm',
+    format: 'cjs',
   },
   plugins: [
     babel({
       exclude: 'node_modules/**'
-    })
+    }),
+    resolve(),
+    commonjs(),
+    dev && run()
   ]
 };
